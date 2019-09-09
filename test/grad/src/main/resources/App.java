7eb09b49a1513d274a7434ed8b708864c8367d5a
Stefan Magnus Xara Brazil Fongen <smfongen@stud.ntnu.no>
2019-09-09 09:50:53 +0200

    Replace App.java

--- test/grad/src/main/resources/App.java
+++ test/grad/src/main/resources/App.java
@@ -1,13 +1,8 @@
 package app;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class App extends Application{
