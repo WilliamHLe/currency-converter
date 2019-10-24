cdb83b79b5217ddf53c0c6a13fae3e15628ca455
Stefan <smfongen@stud.ntnu.no>
2019-10-24 11:24:20 +0200

    Oppdatering

--- core/src/main/java/valutaKalk/core/AppIO.java
+++ core/src/main/java/valutaKalk/core/AppIO.java
@@ -1,13 +1,19 @@
 package valutaKalk.core;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class AppIO implements AppIOInterface {
+
+	JSONObject obj = new JSONObject();
+	public static String valuta1;
+	public static String valuta2;
+	public static double valuta1amount;
+	public static double valuta2amount;
 
 	@Override
 	public void save(String filename, Valuta nok, Valuta result, String old, String ny) throws IOException {
@@ -71,6 +77,26 @@ public class AppIO implements AppIOInterface {
 
 
 
+	}
+
+	@Override
+	public JSONObject saveJSON(String nok, String result, double old, double ny) throws IOException{
+		obj = JSON.ValtutaJSON(nok, result, old, ny);
+
+		PrintWriter pw = new PrintWriter("valuta.json");
+		pw.write(obj.toJSONString());
+		pw.flush();
+		pw.close();
+		return null;
+	}
+
+	public void loadJSON() throws IOException, ParseException {
+		Object obj = new JSONParser().parse(new FileReader("valuta.json"));
+		JSONObject info = (JSONObject) obj;
+		valuta1 = (String) info.get("valuta1");
+		valuta2 = (String) info.get("valuta2");
+		valuta1amount = (double) info.get("valuta1amount");
+		valuta2amount = (double) info.get("valuta2amount");
 	}
 
 }
